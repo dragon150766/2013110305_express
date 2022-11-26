a1ff25ffd4accb13b81d021e0a9487bffa17b383
dragon150766 <dragon150766@hotmail.com>
2022-11-26 11:20:20 +0700

    Week03_WorkshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Lest\'s Battle Beg50in');
+  res.status(200).json({
+    fullname:'Siripong Poolnuch'
+  })
 });
+
+router.get('/bio', function(req, res, next){
+  res.status(200).json({
+    fullname:'Siripong Poolnuch',
+    nickname:'Fluk',
+    hobby:'Sleep',
+    gitusername:'dragon150766'
+  })
+})
+
 
 module.exports = router;
